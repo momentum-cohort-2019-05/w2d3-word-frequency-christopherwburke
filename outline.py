351e5f2dd447a396f4dd801797c3867ee8abcb85
christopherwburke <christopher.w.burke@gmail.com>
2019-06-05 22:48:09 -0400

    First Commit, so much to learn. I am so behind on basic Python syntax. Strangely I retained so little from the first two days this week that I feel like I missed two days of class.

---
+++ outline.py
@@ -0,0 +1,24 @@
+#put code here and copy it into word_frequency.py
+
+# need to (import?) the EP file and convert it into a string?
+# probably using the "open" command/function
+
+with open("emancipation_proclamation.txt", "r") as f:
+    x.split()
+    x.lowercase()
+
+#can i import it as a string or do i import it and then convert it to a string?
+
+# now the text file is split into individual words, all lowercase
+
+# need to declare an empty dictionary, this is ultimately where the results will go
+
+ep_dict = {}
+
+# assign keys and values to the dictionary? i.e. words and number of times they appear
+# syntax is mydict[key] = "value"
+?emanproc_dict[] = ?
+
+# need to figure out what "pass a list" means
+
+# have to use key to sort by the number of times word appears
